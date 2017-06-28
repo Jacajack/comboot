f2a0ee0ab005bebe9a6fb94c19d1de01fd35298e
Jacek Wieczorek <mrjjot@gmail.com>
2017-06-28 23:18:31 +0200

    Add routine for clearing screen

--- asm/stdio.asm
+++ asm/stdio.asm
@@ -28,3 +28,14 @@ puts:
 	popa
 	popf
 	ret
+
+;Clear screen in text mode
+cls:
+	pushf
+	pusha
+	mov al, 0x02	;Change gfx mode to text mode (that should be enough)
+	mov ah, 0		;
+	int 0x10		;
+	popa
+	popf
+	ret
