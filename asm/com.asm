dce4367403ba6c9d7a305d76e0cc7529e081224b
Jacek Wieczorek <mrjjot@gmail.com>
2017-06-29 12:43:47 +0200

    Add some more serial port routines (comwrecv, comclibuf) and comments

--- asm/com.asm
+++ asm/com.asm
@@ -43,28 +43,64 @@ cominit:
 	com_div: dw 0
 	com_conf: db 0
 
+;Checks if there are any characters awaiting in COM buffer
+;return al - buffer status (0-empty, 1-awaiting characters)
 comckin:
 	pushf
 	pusha
-	mov dx, [com_addr]
-	add dx, 5
-	in ax, dx
-	and ax, 0x01
-	mov [comckin_b], al
-	popa
-	mov al, [comckin_b]
+	mov dx, [com_addr]		;Access serial port
+	add dx, 5				;
+	in ax, dx				;Read IRQs register
+	and ax, 0x01			;Get youngest bit
+	mov [comckin_b], al		;Store it in RAM
+	popa					;
+	mov al, [comckin_b]		;And bring back from it
 	popf
 	ret
 	comckin_b: db 0
 
+;Receives byte from serial port
+;return al - received byte
 comrecv:
 	pushf
 	pusha
-	mov dx, [com_addr]
-	in ax, dx
-	mov [comrecv_b], al
-	popa
-	mov al, [comrecv_b]
+	mov dx, [com_addr]		;Access serial port
+	in ax, dx				;Read data register
+	mov [comrecv_b], al		;Store char in RAM
+	popa					;
+	mov al, [comrecv_b]		;Bring the char back
 	popf
 	ret
 	comrecv_b: db 0
+
+;Receives byte from serial port (with waiting)
+;return al - received byte
+comwrecv:
+	pushf
+	pusha
+	comwrecv_l:				;
+		call comckin		;Check buffer
+		cmp al, 0			;
+		je comwrecv_l		;Loop until buffer is not empty
+	call comrecv			;Receive character
+	mov [comwrecv_b], al	;Temporarily store character in RAM
+	popa					;Restore all registers
+	mov al, [comwrecv_b]	;Write character to al
+	popf
+	ret
+	comwrecv_b: db 0
+
+;Clears serial port input buffer
+comclibuf:
+	pushf
+	pusha
+	comclibuf_l:			;
+		call comckin		;Check buffer
+		cmp al, 0			;
+		je comclibuf_end	;Loop until buffer is empty
+		call comrecv		;Receive and ignore character from buffer
+		jmp comclibuf_l		;
+	comclibuf_end:
+	popa
+	popf
+	ret
