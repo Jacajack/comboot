43a9a45b161ae883443eb6f923016243753e55a1
Jacek Wieczorek <mrjjot@gmail.com>
2017-06-29 20:32:09 +0200

    WIP commit

--- asm/comboot.asm
+++ asm/comboot.asm
@@ -1,101 +1,167 @@
 [bits 16]
 [org 0x500]
 
+pop dx
+mov [boot_drive], dl
+
 ;Display greeting message
 call cls
 mov si, mesg_greeting
 call puts
-mov si, mesg_anykey
+
+;Display available memory size
+mov si, mesg_mem
+call puts
+int 0x12
+call puthexw
+mov si, mesg_mem2
 call puts
 
-
-;Configure serial
-mov dx, [port_addr]			;Disable interrupts
-add dx, 1					;
-mov al, 0					;
-out dx, al					;
-mov dx, [port_addr]			;Enable baud rate divisor
-add dx, 3					;
-mov al, 0x80				;
-out dx, al					;
-mov dx, [port_addr]			;Set baud rate divisor
-add dx, 0					;
-mov al, [port_conf_div]		;
-out dx, al					;
-mov dx, [port_addr]			;Baud rate divisor high byte
-add dx, 1					;
-mov al, [port_conf_div + 1]	;
-out dx, al					;
-mov dx, [port_addr]			;No parity, data: 8, stop: 1
-add dx, 3					;
-mov al, 0x03				;
-out dx, al					;
-mov dx, [port_addr]			;FIFO enabled, 14-byte threshold
-add dx, 2					;
-mov al, 0xc7				;
-out dx, al					;
-mov dx, [port_addr]			;IRQs - RTS, DSR
-add dx, 4					;
-mov al, 0x0b				;
-out dx, al					;
-
-call getc
-
-mov si, mesg_ok
+;Display message telling user to insert new floppy disk
+mov si, mesg_newfloppy
 call puts
 
-aa:
-call comckin
-cmp al, 0
-je aa
-call comrecv
+;Wait for keypress
+;call getc
+
+;Init serial port
+mov ax, 0x3f8
+mov bx, 12
+mov cl, 0x03
+call cominit
+call comclibuf
+
+;Receiving begins
+mov si, mesg_recv
+call puts
+
+mov ax, 0x0000
+mov es, ax
+mov ax, 0x0001
+
+mov bx, 0x0
+mov fs, bx
+mov bx, 0x000f
+
+mov dx, 0x0010
+
+call memcrawl
+mov bx, ax
+mov ax, es
+call puthexw
+mov al, ':'
 call putc
-
-jmp aa
+mov ax, bx
+call puthexw
 
 jmp $
 
-comckin:
+;es:ax - current address
+;fs:bx - end address
+;dx - offset
+;return es:ax - new address
+;return cl - crawl performed (0 - ok)
+memcrawl:
 	pushf
 	pusha
-	mov dx, [port_addr]
-	add dx, 5
-	in ax, dx
-	and ax, 0x01
-	mov [comckin_b], al
-	popa
-	mov al, [comckin_b]
+	push dx					;
+							;Calculate real address of given pointer
+	mov dx, es				;Load segment into dx
+	shr dh, 4				;Get oldest 4 bits of segment number
+	mov cl, dh				;Store them in cl
+	mov dx, es				;Load es into dx again
+	shl dx, 4				;Shift segment register 4 bytes to the left
+	add ax, dx				;Add it to offset
+	adc cl, 0				;Increment oldest address byte depending on carry flag
+	mov [memcrawl_c+2], cl	;Update values in memory
+	mov [memcrawl_c], ax	;
+							;Calculate real address of end pointer
+	mov dx, fs				;Load segment into dx
+	shr dh, 4				;Get oldest 4 bits of segment number
+	mov ch, dh				;Store them in ch
+	mov dx, fs				;Load es into dx again
+	shl dx, 4				;Shift segment register 4 bytes to the left
+	add bx, dx				;Add it to offset
+	adc ch, 0				;Increment oldest address byte depending on carry flag
+	mov [memcrawl_e+2], ch	;Update values in memory
+	mov [memcrawl_e], bx	;
+	sub bx, ax				;Substract both pointers to calculate maximum allowed jump
+	sbb ch, cl				;
+	pop dx					;Get step size
+	mov cl, 1				;Assume crawl to be failed
+	mov [memcrawl_bnd], cl	;
+	mov cl, [memcrawl_c+2]	;Get original pointer from memory
+	mov ax, [memcrawl_c]	;
+	cmp ch, 0				;Compare requested step with distance to end pointer
+	jne memcrawl_jok		;
+	cmp dx, bx				;
+	jb memcrawl_jok			;
+	mov bx, 0				;If pointer cannot be incremented
+	mov ch, 0				;
+	jmp memcrawl_jbad		;
+	memcrawl_jok:			;Increment pointer
+	mov ch, 0				;Crawl successfull
+	mov [memcrawl_bnd], ch	;
+	add ax, dx				;Add jump value with carry
+	adc cl, 0				;
+	memcrawl_jbad:			;Pointer cannot be incremented
+	mov bx, 0				;Copy 4 oldest address bits
+	shl cl, 4				;To highest bits of segment register
+	mov bh, cl				;
+	mov dx, ax				;Get 4 youngest bits of real address as offset
+	and dx, 0xF				;
+	shr ax, 4				;Shift the rest 4 bits to the right and add it to segment register
+	add bx, ax				;
+	jnc memcrawl_nocarry	;If no carry flag is set - exit
+	shl bx, 4				;Shift segment register 4 bits to the left
+	add dx, bx				;Add it to offset register
+	mov bx, 0xffff			;Fill segment register with highest value possible
+	memcrawl_nocarry:		;
+	mov [memcrawl_seg], bx	;Store segment
+	mov [memcrawl_off], dx	;Store offset
+	popa					;
+	mov ax, [memcrawl_seg]	;Restore all values that should be retuned from memory
+	mov es, ax				;
+	mov ax, [memcrawl_off]	;
+	mov cl, [memcrawl_bnd]	;
 	popf
 	ret
-	comckin_b: db 0
+	memcrawl_c: times 3 db 0
+	memcrawl_e: times 3 db 0
+	memcrawl_seg: dw 0
+	memcrawl_off: dw 0
+	memcrawl_bnd: db 0
 
-comrecv:
-	pushf
-	pusha
-	mov dx, [port_addr]
-	in ax, dx
-	mov [comrecv_b], al
-	popa
-	mov al, [comrecv_b]
-	popf
-	ret
-	comrecv_b: db 0
 
-;Chosen serial port address
-port_addr: dw 0x3f8
-port_conf_div: dw 1
+
+boot_drive: db 0
 
 ;Messages collection
 mesg_greeting:
-	db "comboot v0.1", 10, 13
+	db "---comboot v0.2", 10, 13
 	db 0
 
-mesg_anykey:
-	db "please press any key to continue...", 10, 13
+mesg_mem:
+	db "available memory: "
+	db 0
+mesg_mem2:
+	db "h KB", 10, 13
+	db 0
+
+mesg_newfloppy:
+	db "please insert new flopyy disk and press any key afterwards...", 10, 13
+	db 0
+
+mesg_recv:
+	db "receiving data at 9600 baud, 8 bits data, 1 bit stop...", 10, 13
 	db 0
 
 mesg_ok:
 	db "ok...", 10, 13
 	db 0
 
+mesg_nl: db 10, 13, 0
+
 %include "stdio.asm"
+%include "com.asm"
+%include "disk.asm"
